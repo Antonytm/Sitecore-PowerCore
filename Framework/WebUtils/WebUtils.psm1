3cf812ec1e0e7c0adb00772162f1251199f060a2
Antonytm <antontishchenko@gmail.com>
2014-01-15 12:37:31 +0200

    Writing to log of exceptions on requests to site.
    There was difficulties to find problems when there was exception on
    sites after requests from build script. User saw error message with no
    details that could be useful. Now, on web exception script writes
    response string and then throws exception.

--- Framework/WebUtils/WebUtils.psm1
+++ Framework/WebUtils/WebUtils.psm1
@@ -7,9 +7,25 @@ function Get-WebPage {
 	}
 	
 	Write-Output "Creating request to $url"
-	$request = [system.net.WebRequest]::Create($url)
-	$request.Timeout = 2400000
-	return $request.GetResponse()		
+	
+	Try
+	{
+		$request = [system.net.WebRequest]::Create($url)
+		$request.Timeout = 2400000
+		return $request.GetResponse()		
+	}
+	Catch [System.Net.WebException]
+	{
+		#On web exception write response string and throw exception
+		$stream = $_.Exception.Response.GetResponseStream()
+		$reader = new-object -typename System.IO.StreamReader -argumentlist $stream
+		Write-Host $reader.ReadToEnd()
+		Throw $_.Exception
+	}
+	Catch
+	{
+		Throw $_.Exception
+	}
 }
 
 Export-ModuleMember -function Get-WebPage
